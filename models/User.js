75897360c834c795679e20b5162e77ebc4fa9d4f
kimnewbie <gracecancho@gmail.com>
2022-08-19 11:22:31 +0900

    #5 User validation

--- models/User.js
+++ models/User.js
@@ -10,6 +10,10 @@ const UserSchema = new Schema(
             type: String,
             required: true,
         },
+        // confirmPassword: {
+        //     type: String,
+        //     required: true,
+        // },
         name: {
             type: String,
             required: true,
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const validateRegisterInput = require("../validation/registerValidation");
 
 // @route   GET /api/auth/test
 // @desc    Test the auth route
@@ -15,6 +16,22 @@ router.get("/test", (req, res) => {
 // @access  Public
 router.post("/register", async (req, res) => {
     try {
+        const { errors, isValid } = validateRegisterInput(req.body);
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        // check for existing user
+        const existingEmail = await User.findOne({
+            // email: req.body.email
+            // 대소문자 
+            email: new RegExp("^" + req.body.email + "$", "i")
+        });
+
+        if (existingEmail) {
+            return res.status(400).json({ error: 'There is already a user with this email' });
+        }
+
         // hash the password
         const hashedPassword = await bcrypt.hash(req.body.password, 12);
         // create a new user
